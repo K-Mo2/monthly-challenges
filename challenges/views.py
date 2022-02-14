80a820a7db5e59ea5fec6febd186d9e5fa70225b
K-Mo2 <karimelsisi27@gmail.com>
2022-02-14 16:31:23 +0200

    adding urls. views and dynamic path segments

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
+def monthly_challenges(request, month):
+    challenge_text=None
+    if month == 'january':
+        challenge_text='Eat no meat for the entire month!'
+    
+    elif month == 'february':
+        challenge_text='Walk for at least 20 minutes everyday!'
+    
+    elif month == 'march':
+        challenge_text='Learn Django for at least 20 minutes everyday!'
+    
+    else:
+        return HttpResponseNotFound("This month is not supprted!")
+    
+    return HttpResponse(challenge_text)
