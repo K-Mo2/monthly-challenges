e825d72cfea5493123e3a7bd840c604f0537c6d2
K-Mo2 <karimelsisi27@gmail.com>
2022-02-14 18:11:09 +0200

    adding dynamic view logic

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 monthly_challenges = {
@@ -10,6 +11,18 @@ monthly_challenges = {
     'may':'Eat vegetables for 30 days',
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalize_months = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li> <a href='{month_path}'>{capitalize_months}</a></li>"
+        
+    response_data = f"<ol>{list_items}</ol>"
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -18,7 +31,8 @@ def monthly_challenge_by_number(request, month):
     
     redirect_month = months[month - 1]    
 
-    return HttpResponseRedirect('/challenges/' + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
