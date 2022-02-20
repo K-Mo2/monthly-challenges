3e9b795fc763e1ccd4d29114518dd18763c27ee1
K-Mo2 <karimelsisi27@gmail.com>
2022-02-20 17:00:31 +0200

    adding django-rest-framewrok app

--- monthly_challenges/urls.py
+++ monthly_challenges/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from quickstart.views import UserViewSet, GroupViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r"users", UserViewSet)
+router.register(r"groups", GroupViewSet)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('challenges/', include("challenges.urls")),
+    path('api-auth/', include("rest_framework.urls", namespace="rest_framework")),
 ]
